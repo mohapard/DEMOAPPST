c09583fc5f131d29c4ae4c0b43fe293ffde821b8
mohapard <mohapard@gmail.com>
2022-11-20 13:39:41 +0200

    Create 05_☎️_Contact_Us.py

---
+++ pages/05_☎️_Contact_Us.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import components.authenticate as authenticate
+
+st.set_page_config(
+    page_title="Contact Us",
+    page_icon="👾",
+    layout="wide",
+)
+
+
+# Check authentication when user lands on the home page.
+authenticate.set_st_state_vars()
+
+# Add login/logout buttons
+if st.session_state["authenticated"]:
+    authenticate.button_logout()
+else:
+    authenticate.button_login()
+
+st.markdown("COMING SOON ! ")
